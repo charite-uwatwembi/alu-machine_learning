89af386ca32e6cd18af8f576efde0787ff0dd55d
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-05-17 21:16:34 -0700

    more commits

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+
+n def np_matmul(mat1, mat2)
+    that performs matrix multiplication
+'''
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    '''
+        np_matmul Multiply two Matrices
+    '''
+    return np.matmul(mat1, mat2)
