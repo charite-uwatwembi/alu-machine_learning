5546b7d380c816fac3e62c18f83df2287653b0f4
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-06-01 14:38:16 +0200

    9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-This module provides a function to calculate 
-the sum of squares 
+This module provides a function to calculate,
+the sum of squares,
 of the first n natural numbers.
 """
 
