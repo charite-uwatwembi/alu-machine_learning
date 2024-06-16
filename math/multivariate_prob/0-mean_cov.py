77ba438c27aaaef8269c8333060a7ba12597b1b6
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-06-16 10:36:27 +0200

    0-mean_cov.py

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
+""" Implements mean_cov
+"""
+import numpy as np
 
-if __name__ == '__main__':
-    import numpy as np
-    mean_cov = __import__('0-mean_cov').mean_cov
 
-    np.random.seed(0)
-    X = np.random.multivariate_normal(
-        [12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 10000)
-    mean, cov = mean_cov(X)
-    print(mean)
-    print(cov)
-    
+def mean_cov(X):
+    """ Calculates the mean and covariance of a matrix
+    Args:
+        X (numpy.ndarray) : A 2D array representing the dataset
+    Returns:
+        mean (numpy.ndarray): The mean vector of the dataset
+        cov (numpy.ndarray): A 2D array representing the covariance
+        of the dataset
+    """
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise TypeError('X must be a 2D numpy.ndarray')
+
+    n, _ = X.shape
+
+    if n < 2:
+        raise ValueError('X must contain multiple data points')
+
+    mean = np.mean(X, axis=0).reshape(1, -1)
+    deviations = X - mean
+    cov = (deviations.T @ deviations) / (n - 1)
+
+    return mean, cov
