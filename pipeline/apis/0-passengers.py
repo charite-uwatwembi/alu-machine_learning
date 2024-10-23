c33e42123976f976752042f89539ce5c66e0decb
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-10-23 11:47:00 +0200

    0-passengers.py

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,33 +1,25 @@
 #!/usr/bin/env python3
+
 """
-Fetches a list of ships from the Swapi API that can hold at least a given number of passengers.
-"""
-import requests
+This module contains a function that
+uses Swapi API to get the list of ships that
+can hold given number of passengers"""
+
 
 def availableShips(passengerCount):
-    
-    url = 'https://swapi.dev/api/starships/'
+    """return empty list if no ships carry that no.
+    of passengers else return list of ships"""
+    import requests
+    url = "https://swapi-api.alx-tools.com/api/starships/"
+    response = requests.get(url)
+    data = response.json()
     ships = []
-    
-    while url:  # Keep fetching as long as there's a 'next' page
-        response = requests.get(url)
-        data = response.json()
-        
-        for ship in data['results']:
-            passengers = ship['passengers'].replace(',', '')  # Remove commas in large numbers
-            if passengers.isdigit() and int(passengers) >= passengerCount:
-                ships.append(ship['name'])
-        
-        # Go to the next page
-        url = data['next']
-    
+    # print(data["results"])
+    for result in data['results']:
+        if result['passengers'] != "n/a":
+            passengers_no = int(result['passengers'].replace(',', ''))
+            # print(passengers_no)
+            if passengers_no >= passengerCount:
+                ships.append(result['name'])
+
     return ships
-
-
-if __name__ == "__main__":
-    """
-    Test the availableShips function with different passenger counts.
-    """
-    ships = availableShips(4)
-    for ship in ships:
-        print(ship)
