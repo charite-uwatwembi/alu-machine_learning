e9428aa4a5cd2e76fe8e9a997e675c28e1cdce92
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-10-26 21:08:42 +0200

    apis

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -1,22 +1,30 @@
-#!/usr/bin/env python3
-"""Pipeline Api"""
+#!/usr/bin/env python
+"""Pipeline Apis"""
 import requests
 
-
 if __name__ == '__main__':
-    """pipeline api"""
+    """pipeline apis"""
     url = "https://api.spacexdata.com/v4/launches"
     r = requests.get(url)
-    rocket_dict = {"5e9d0d95eda69955f709d1eb": 0}
+    rocket_dict = {}
 
+    # Count launches per rocket ID
     for launch in r.json():
-        if launch["rocket"] in rocket_dict:
-            rocket_dict[launch["rocket"]] += 1
+        rocket_id = launch["rocket"]
+        if rocket_id in rocket_dict:
+            rocket_dict[rocket_id] += 1
         else:
-            rocket_dict[launch["rocket"]] = 1
-    sorted_arr = {}
-    for key, value in sorted(rocket_dict.items(),
-                             key=lambda kv: kv[1], reverse=True):
+            rocket_dict[rocket_id] = 1
+
+    # Sorted dictionary of rocket counts
+    sorted_rockets = sorted(rocket_dict.items(), key=lambda kv: kv[1], reverse=True)
+    
+    # Print results with rocket names, and adding a newline at the end of each line
+    output = ""
+    for key, value in sorted_rockets:
         rurl = "https://api.spacexdata.com/v4/rockets/" + key
         req = requests.get(rurl)
-        print(req.json()["name"] + ": " + str(value))
+        rocket_name = req.json().get("name", "Unknown Rocket")
+        output += f"{rocket_name}: {value}\n"
+
+    print(output.strip())
