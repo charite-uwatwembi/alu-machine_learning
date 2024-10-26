804cf5950a1fe4d6a43a89c413fbff1dc569bb29
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-10-26 20:52:30 +0200

    4-rocket_frequency.py

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -2,28 +2,21 @@
 """Pipeline Api"""
 import requests
 
+
 if __name__ == '__main__':
     """pipeline api"""
     url = "https://api.spacexdata.com/v4/launches"
     r = requests.get(url)
+    rocket_dict = {"5e9d0d95eda69955f709d1eb": 0}
 
-# Dictionary to count launches by rocket ID dynamically
-    rocket_dict = {}
-
-    # Count launches per rocket ID
     for launch in r.json():
-        rocket_id = launch["rocket"]
-        if rocket_id in rocket_dict:
-            rocket_dict[rocket_id] += 1
+        if launch["rocket"] in rocket_dict:
+            rocket_dict[launch["rocket"]] += 1
         else:
-            rocket_dict[rocket_id] = 1
-
-    # Sorted dictionary of rocket counts
-    sorted_rockets = sorted(rocket_dict.items(), key=lambda kv: kv[1], reverse=True)
-    
-    # Print results with rocket names
-    for key, value in sorted_rockets:
+            rocket_dict[launch["rocket"]] = 1
+    sorted_arr = {}
+    for key, value in sorted(rocket_dict.items(),
+                             key=lambda kv: kv[1], reverse=True):
         rurl = "https://api.spacexdata.com/v4/rockets/" + key
         req = requests.get(rurl)
-        rocket_name = req.json().get("name", "Unknown Rocket")
-        print(f"{rocket_name}: {value}")
+        print(req.json()["name"] + ": " + str(value))
