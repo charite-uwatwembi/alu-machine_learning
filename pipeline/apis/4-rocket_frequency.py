880c77093a7a0f902487eb1e12cd5a97d674f766
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-10-26 13:24:09 -0700

    4-rocket_frequency.py

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -39,4 +39,4 @@ def get_launch_count_by_rocket():
 if __name__ == "__main__":
     rocket_launch_counts = get_launch_count_by_rocket()
     for rocket_name, count in rocket_launch_counts:
-        print("{}: {}".format(rocket_name, count)) 
+        print("{}: {}".format(rocket_name, count))
