c2a3e54cb28fcd5bf08fc94e7b46ae5a53cbd1bf
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-11-02 21:21:53 +0200

    17-store.sql

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,13 @@
+-- creates a trigger that decreases a uantity
+DROP TRIGGER IF EXISTS decrease_quantity;
+
+DELIMITER $$
+CREATE TRIGGER decrease_quantity
+       AFTER INSERT
+       ON `orders` FOR EACH ROW
+BEGIN
+        UPDATE items SET quantity = quantity - new.number
+        WHERE items.name=new.item_name;
+END $$
+
+DELIMITER ;
